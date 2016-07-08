29cd2dffad545da3261248b66699fbd6fb6c77e2
eekidu <wosyou@qq.com>
2016-07-08 09:10:38 +0800

    数据库继承

--- src/TestCase.java
+++ src/TestCase.java
@@ -1,19 +1,15 @@
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 /**
  * Created by wosyo on 2016/7/7.
  */
 public class TestCase {
-
     @Test
     public void funcation() throws Exception {
-
         DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp","root","1234");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp", "root", "1234");
         Statement statement = connection.createStatement();
         String sql = "INSERT INTO `emp`.`t_emp` (\n" +
                 "  `id`,\n" +
@@ -33,5 +29,22 @@ public class TestCase {
         connection.close();
         System.out.println("success");
     }
+
+    @Test
+    public void funQuery() throws SQLException {
+        DriverManager.registerDriver((new com.mysql.jdbc.Driver()));
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp", "root", "1234");
+        Statement statement = conn.createStatement();
+        String sql = "select * from t_emp;";
+        ResultSet resultSet = statement.executeQuery(sql);
+        while (resultSet.next()){
+            System.out.println(resultSet.getString("username"));
+        }
+        statement.close();
+        conn.close();
+        System.out.println("success");
+    }
+
+
 
 }
